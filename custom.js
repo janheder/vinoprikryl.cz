92d46410b154a1ac94c9464720f378825a59bbb5
Jan Heder <info@janheder.cz>
2022-05-12 22:37:55 +0200

    .

---
+++ custom.js
@@ -0,0 +1,10 @@
+// -----------------------------------------------------------------------------
+// PRODUCT PAGE
+// -----------------------------------------------------------------------------
+
+/* relocate product title */
+$(".p-detail-info .flags").insertBefore(".p-final-price-wrapper");
+
+$(".p-detail-inner-header").insertBefore(".p-final-price-wrapper");
+
+$(".p-detail-info .stars-wrapper").insertBefore(".p-final-price-wrapper");
