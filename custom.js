27ab065dbc718ef76e1bb981c180ccceba8a65c4
Jan Heder <info@janheder.cz>
2022-05-25 00:58:26 +0200

    .

--- custom.js
+++ custom.js
@@ -8,3 +8,7 @@ $(".p-detail-info .flags").insertBefore(".p-final-price-wrapper");
 $(".p-detail-inner-header").insertBefore(".p-final-price-wrapper");
 
 $(".p-detail-info .stars-wrapper").insertBefore(".p-final-price-wrapper");
+
+
+
+$(".p .p-tools form").prepend('<span class="quantity"><input type="text" value="1" name="amount" class="form-control amount" data-min="1" data-max="100" tabindex="0"><span class="increase"></span><span class="decrease"></span></span>');
