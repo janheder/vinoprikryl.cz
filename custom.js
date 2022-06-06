4285021b32e9c250541f171f64eae5499178517a
Jan Heder <info@janheder.cz>
2022-06-06 11:40:01 +0200

    .

--- custom.js
+++ custom.js
@@ -10,9 +10,9 @@ $(".p-detail-inner-header").insertBefore(".p-final-price-wrapper");
 $(".p-detail-info .stars-wrapper").insertBefore(".p-final-price-wrapper");
 
 
-
-$(".p .p-tools form").prepend('<span class="quantity"><input type="text" value="1" name="amount" class="form-control amount" data-min="1" data-max="100" tabindex="0"><span class="increase"></span><span class="decrease"></span></span>');
-
+document.addEventListener('ShoptetDOMPageContentLoaded', function () {
+    $(".p .p-tools form").prepend('<span class="quantity"><input type="text" value="1" name="amount" class="form-control amount" data-min="1" data-max="100" tabindex="0"><span class="increase"></span><span class="decrease"></span></span>');
+});
 
 $(".navigation-buttons").prepend($(".top-navigation-tools"));
 
