0396debc3ff8937f92b4979afc36edb6f022d8c4
Jan Heder <info@janheder.cz>
2022-08-31 16:23:30 +0200

    .

--- custom.js
+++ custom.js
@@ -103,3 +103,49 @@ if ($(".type-detail").length){
     });
 
 }
+
+
+
+if ($(".add-to-cart-button").length){
+
+    $("body").append('<div class="advancedModal --product"><div class="advancedModal__inner"><h2 class="advancedModal__title">Zboží bylo přidáno do košíku</h2><div class="advancedModal__content"></div><div class="advancedModal__buttons"><a href="/" class="btn btn-ghost">Zpět do obchodu</a><a href="/kosik" class="btn">Přejít do košíku</a></div></div></div>');
+
+    
+    /* call functions after order modal loaded */
+    $(".add-to-cart-button").on('click',function(){
+
+        var img = $(this).closest(".p").find(".image").html();
+        var name = $(this).closest(".p").find(".name").html();
+        var stock = $(this).closest(".p").find(".availability").html();
+
+        var amount = parseFloat($(this).closest(".p").find(".amount").val());
+
+        var priceSingle = $(this).closest(".p").find(".price-final strong").html();
+        var priceTotal = parseFloat(priceSingle.replace(/ /g, ''))*amount;
+
+        document.addEventListener('ShoptetCartUpdated', function () {
+            $("body").addClass("--advancedModal");
+            $(".advancedModal__content").html("");
+        
+            $(".advancedModal__content").prepend('<div class="advancedProduct">' +
+            '<div class="advancedProduct-img">' + img + '</div>' +
+            '<div class="advancedProduct-content">' +
+            '<div class="advancedProduct-name">' + name + '</div>' +
+            '<div class="advancedProduct-stock">Dostupnost<span>' + stock + '</span></div>' +
+            '<div class="advancedProduct-amount">Počet kusů<span>' + amount + 'x</span></div>' +
+            '<div class="advancedProduct-priceTotal">Celková cena<span>' + priceTotal + ' Kč</span></div>' +
+            '</div></div>');
+        },{
+            passive: true
+        });
+    });
+
+
+
+
+    $('.advancedModal').on('click',function(e){
+        if (e.target !== this)
+        return;
+        $("body").removeClass("--advancedModal");
+    });
+}
